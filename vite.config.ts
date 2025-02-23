bd117a14844b331e6a775cdef2f095b88e9657a1
Zakaria Houdache <zhoudache@gmail.com>
2025-02-23 08:29:03 +0000

    x

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,13 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    hmr: {
+      host: "c3386b4d-432a-4f35-a193-b8f809688b4e.lovableproject.com",
+    },
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
